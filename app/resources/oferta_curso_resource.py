7b13041efa4eaf3c6a74c175f614af737fb524a9
saulbarrios <barriossaul@gmail.com>
2020-11-28 19:11:55 -0500

    actualizacion oferta curso

---
+++ app/resources/oferta_curso_resource.py
@@ -0,0 +1,49 @@
+from flask import make_response, jsonify
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required
+from flask_apispec import marshal_with, doc, use_kwargs
+from flask_apispec.views import MethodResource
+from marshmallow import Schema, fields
+from app.models.curso import Curso
+from app.models.categoria import Categoria
+from app.models.oferta_curso import Oferta_curso
+
+# Necesita en Header Bearer: Token (Este es token generado en el login)
+class Oferta_CursoResponseSchema(Schema):
+    message = fields.Str(default='Listado de Cursos Ofertados')
+    data = []
+
+class Oferta_CursoApi(MethodResource,Resource):
+    @doc(description='API Oferta_Curso.', tags=['Lista_Oferta'])
+    @marshal_with(Oferta_CursoResponseSchema)
+    #@jwt_required # proteger a la vista
+    def get(self):
+        try:
+            oferta = Oferta_curso.query.all()
+            lista = []
+
+            for i in oferta:
+                curso = Curso.query.filter_by(id=i.curso_id).first()
+                lista.append({
+                    'id': i.id,
+                    'curso_id': i.curso_id,
+                    'curso': {
+                        'name': curso.curso
+                    },
+                    'fechainicio': i.fechainicio,
+                    'fechafin': i.fechafin,
+                    'tema_periodo': i.tema_periodo,
+                    'contenido_tema': i.contenido_tema
+                })
+
+            return make_response(jsonify({
+                'message': 'Listado de Curso Ofertados',
+                'data': lista
+            }), 200)
+        except Exception as e:
+            print(e)
+            return make_response(jsonify({
+                'message': 'Ocurrio un error'
+            }), 500)
+
+#crear un metodo donde 
--- app/routes/index.py
+++ app/routes/index.py
@@ -2,6 +2,7 @@ from app import api, docs
 from app.resources.auth_resource import LoginApi, SignupApi
 from app.resources.test_resource import TestApi
 from app.resources.curso_resource import CursoApi
+from app.resources.oferta_curso_resource import Oferta_CursoApi
 
 def initialize_routes(api_rest, doc_rest):
     # Registro
@@ -15,6 +16,10 @@ def initialize_routes(api_rest, doc_rest):
     # Curso
     api_rest.add_resource(CursoApi, '/api/cursos')
     doc_rest.register(CursoApi)
+
+    #Oferta Cursos
+    api_rest.add_resource(Oferta_CursoApi, '/api/Oferta_Curso')
+    doc_rest.register(Oferta_CursoApi)
 
 
 initialize_routes(api, docs)
